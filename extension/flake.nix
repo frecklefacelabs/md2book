afd7b111d01b0f352e02574567e54b59e33f615a
Freckleface Cogswell <jeffcogs@gmail.com>
2026-03-28 16:09:56 -0600

    feat: add vsce automatically

---
+++ extension/flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "Node.js development environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            nodejs
+            vsce
+            nodePackages.npm
+          ];
+
+          shellHook = ''
+            echo "Node $(node --version) / npm $(npm --version)"
+          '';
+        };
+      }
+    );
+}
